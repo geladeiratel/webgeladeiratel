871d6f2537316ada70e7bf3275aa4297419bd814
luciorocha <45665129+luciorocha@users.noreply.github.com>
2020-09-07 16:51:01 -0300

    Atualizado.

--- web/index.php
+++ web/index.php
@@ -10,6 +10,8 @@ $app->register(new Silex\Provider\MonologServiceProvider(), array(
   'monolog.logfile' => 'php://stderr',
 ));
 
+   
+
 // Register view rendering
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
     'twig.path' => __DIR__.'/views',
